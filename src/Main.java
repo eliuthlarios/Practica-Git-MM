bc92aa97eee3ded8a121006496e2312b4b0a5a9e
eliuthlarios <eliuthlario@gmail.com>
2024-03-27 16:08:41 -0600

    Otra prueva en la nueva rama

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Perrito.setName("Sussy");
         System.out.println("prueva Repositorio");
         System.out.println("prueva Nueva rama feature/one");
+        System.out.println("prueva de cambios en rama nueva");
         System.out.println("Mi nombre es: " + Perrito.getName());
     }
 }
